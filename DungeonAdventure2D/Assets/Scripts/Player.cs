74ec2423dbf8e71f0691049c71b52f6ceff877c3
NhatMinh <nhatminh0523@gmail.com>
2025-03-26 23:53:54 +0700

    Update Jump , Flip , Fall Character

--- DungeonAdventure2D/Assets/Scripts/Player.cs
+++ DungeonAdventure2D/Assets/Scripts/Player.cs
@@ -8,13 +8,21 @@ public class Player : MonoBehaviour
     [Header("Movement Properties")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
+
+    private bool canDoubleJump;
+
 
     [Header("Collision Properties")]
-    [SerializeField] private bool isGrounded;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
 
     private float xInput;
+
+    private bool facingRight = true;
+    private int facingDirection = 1;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
@@ -33,6 +41,7 @@ public class Player : MonoBehaviour
         HandleCollision();
         HandleInput();
         HandleMovement();
+        HandleFlip();
         HandleAnimations();
     }
     private void Jump()
@@ -40,31 +49,62 @@ public class Player : MonoBehaviour
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 
-    private void HandleCollision()
+    private void DoubleJump()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        canDoubleJump = false;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, doubleJumpForce);
     }
 
     private void HandleInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            JumpButton();
+        }
+    }
+
+    private void JumpButton()
+    {
+        if(isGrounded)
+        {
+            canDoubleJump = true;
             Jump();
         }
+        else if (canDoubleJump)
+            DoubleJump();
     }
 
     private void HandleMovement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
     }
 
     private void HandleAnimations()
     {
         anim.SetFloat("xVelocity", xInput);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
+    private void HandleFlip()
+    {
+        if (facingRight && rb.linearVelocityX < 0  || !facingRight && rb.linearVelocityX > 0)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+        facingRight = !facingRight;
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x , transform.position.y - groundCheckDistance));
