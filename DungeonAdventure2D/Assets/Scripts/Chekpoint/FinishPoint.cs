6d21363d762fd8ae80946ac2158d1325603b2d2d
NhatMinh <nhatminh0523@gmail.com>
2025-07-08 09:54:24 +0700

    lock and unlock level button

--- DungeonAdventure2D/Assets/Scripts/Chekpoint/FinishPoint.cs
+++ DungeonAdventure2D/Assets/Scripts/Chekpoint/FinishPoint.cs
@@ -6,6 +6,8 @@ public class FinishPoint : MonoBehaviour
     private Animator anim => GetComponent<Animator>();
     private float timeDelay = 0.5f;
 
+    [SerializeField] private int levelIndex;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
@@ -21,7 +23,30 @@ public class FinishPoint : MonoBehaviour
     {
         yield return new WaitForSeconds(timeDelay);
 
+        SaveStars(GameManager.instance.starCollected);
+        UnlockNextLevel();
+
         UIingame.instance.ShowCompletedUI(GameManager.instance.fruitCollected);
     }
 
+    private void SaveStars(int collectedStars)
+    {
+        string key = "Level" + levelIndex + "_Star";
+        int savedStars = PlayerPrefs.GetInt(key, 0);
+
+        if (collectedStars > savedStars)
+        {
+            PlayerPrefs.SetInt(key, collectedStars);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void UnlockNextLevel()
+    {
+        int nextLevel = levelIndex + 1;
+        string nextKey = "Level" + nextLevel + "_Unlocked";
+
+        PlayerPrefs.SetInt(nextKey, 1);
+        PlayerPrefs.Save();
+    }
 }
--- DungeonAdventure2D/Assets/Scripts/UI/LevelButton.cs
+++ DungeonAdventure2D/Assets/Scripts/UI/LevelButton.cs
@@ -1,18 +1,57 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour
 {
+    [SerializeField] private Sprite starOnSprite;
+    [SerializeField] private Sprite starOffSprite;
+    [Space]
+    [Header("Properties")]
+    [SerializeField] private Image[] stars;
     [SerializeField] private TextMeshProUGUI levelNumberText;
     private int levelIndex;
     private string sceneName;
+
+    [SerializeField] private GameObject lockIcon;     
+    [SerializeField] private Button levelButton;  
 
     public void SetUpLevelButton(int newLevelIndex)
     {
         levelIndex = newLevelIndex;
         levelNumberText.text = levelIndex.ToString();
         sceneName = "Level " + levelIndex.ToString();    
+
+        UpdateLockState();
+        UpdateStarsUI();
+    }
+
+    private void UpdateStarsUI()
+    {
+        string key = "Level" + levelIndex + "_Star";
+        int savedStars = PlayerPrefs.GetInt(key, 0);
+
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].sprite = i < savedStars ? starOnSprite : starOffSprite;
+        }
+    }
+
+    private void UpdateLockState()
+    {
+        bool isUnlocked = levelIndex == 1 || PlayerPrefs.GetInt("Level" + levelIndex + "_Unlocked", 0) == 1;
+
+        if (lockIcon != null)
+            lockIcon.SetActive(!isUnlocked);
+
+        if (levelButton != null)
+            levelButton.interactable = isUnlocked;
+
+        foreach (var star in stars)
+        {
+            star.gameObject.SetActive(isUnlocked);
+        }
     }
 
     public void LoadLevel()
